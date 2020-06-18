a9f86e187ccbf144eecd45e4af5c5b91d34d20b7
Vermunds <33235701+Vermunds@users.noreply.github.com>
2020-06-18 18:08:46 +0200

    2.0
    Complete remake.
    Fixed a memory corruption.
    Now supports Address Library for SKSE Plugins and it's compatible with runtime version 1.5.3 and newer.

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# ClassicSprintingRedone-SE
-A mod for the Elder Scrolls V: Skyrim Special Edition.
+# Classic Sprinting Redone - SE
+A mod for The Elder Scrolls V: Skyrim - Special Edition.
 
-This mod will make the sprinting work the same way as it did in old Skyrim. What it makes different from other similar mods that this will modify the code directly, so it doesn't rely on external scripts anymore. It works with keyboards and controllers too.
+This mod will make the sprinting work the same way as it did in old Skyrim. Hold instead of a toggle. It works with keyboards and controllers too.
 
 Available on Nexusmods: https://www.nexusmods.com/skyrimspecialedition/mods/20166
+
+## End User Dependencies
+* [SKSE64](https://skse.silverlock.org/)
+* [Address Library for SKSE Plugins](https://www.nexusmods.com/skyrimspecialedition/mods/32444)
+
+## Build Dependencies
+* [CommonLibSSE](https://github.com/Ryan-rsm-McKenzie/CommonLibSSE)
--- main.cpp
+++ main.cpp
@@ -1,189 +1,150 @@
-#include "skse64_common/skse_version.h"
-#include "skse64_common/Relocation.h"
-#include "skse64_common/BranchTrampoline.h"
+#include "RE/Skyrim.h"
+#include "SKSE/API.h"
+#include "REL/Relocation.h"
 
-#include "skse64/PluginAPI.h"
+#include "version.h"
 
-#include <shlobj.h>
-#include <xbyak/xbyak.h>
+inline constexpr REL::ID SprintHandler_ProcessButton(static_cast<std::uint64_t>(41358));
 
-/*
-	[1.5.39] SkyrimSE.exe + 0x704DF0
-	[1.5.51] SkyrimSE.exe + 0x704B30
-	[1.5.53] SkyrimSE.exe + 0x704B30
-	[1.5.62] SkyrimSE.exe + 0x704B30
-	[1.5.73] SkyrimSE.exe + 0x704940
-	[1.5.80] SkyrimSE.exe + 0x704940
-	[1.5.97] SkyrimSE.exe + 0x704940
-*/
-//0F 57 C0 0F 2E 42 28 75 14 0F 2F 42 2C 77 0E B8 ?? ?? ?? ?? 84 C0
-RelocAddr <uintptr_t *> SprintKeyStatusFunction = 0x704940;
+enum FlagBDD : UInt8
+{
+	kNone = 0,
+	kSprinting = 1 << 0
+	//...
+};
 
-/*
-	[1.5.39] SkyrimSE.exe + 0x2F4DEF8
-	[1.5.51] SkyrimSE.exe + 0x2F4DEF8
-	[1.5.53] SkyrimSE.exe + 0x2F4DEF8
-	[1.5.62] SkyrimSE.exe + 0x2F4DEF8
-	[1.5.73] SkyrimSE.exe + 0x2F26EF8
-	[1.5.80] SkyrimSE.exe + 0x2F26EF8
-	[1.5.97] SkyrimSE.exe + 0x2F26EF8
-*/
+class MenuOpenCloseEventHandler : public RE::BSTEventSink<RE::MenuOpenCloseEvent>
+{
+public:
+	virtual RE::BSEventNotifyControl ProcessEvent(const RE::MenuOpenCloseEvent* a_event, RE::BSTEventSource<RE::MenuOpenCloseEvent>* a_dispatcher) override
+	{
+		RE::PlayerCharacter* player = RE::PlayerCharacter::GetSingleton();
 
-RelocAddr <uintptr_t *> SprintStatusAddress = 0x2F26EF8;
+		if (a_event && a_event->opening && (player->unkBDD & FlagBDD::kSprinting) != FlagBDD::kNone)
+		{
+			player->unkBDD &= ~FlagBDD::kSprinting;
+		}
 
-/*
-	[1.5.39] SkyrimSE.exe + 0x705290
-	[1.5.51] SkyrimSE.exe + 0x704FD0
-	[1.5.53] SkyrimSE.exe + 0x704FD0
-	[1.5.62] SkyrimSE.exe + 0x704FD0
-	[1.5.73] SkyrimSE.exe + 0x704DE0
-	[1.5.80] SkyrimSE.exe + 0x704DE0
-	[1.5.97] SkyrimSE.exe + 0x704DE0
-*/
-//48 89 5C 24 08 57 48 83 EC 30 C6 41 4A 00 48 8B FA F2 0F 10 05 67 D7 90 02
-RelocAddr <uintptr_t *> ControlFunction = 0x704DE0;
+		return RE::BSEventNotifyControl::kContinue;
+	}
+};
+MenuOpenCloseEventHandler g_menuOpenCloseEventHandler;
+
+void SprintHandler_ProcessButton_Hook(RE::SprintHandler* a_this, RE::ButtonEvent* a_event, RE::PlayerControlsData* a_data)
+{
+	RE::PlayerCharacter* player = RE::PlayerCharacter::GetSingleton();
+
+	if (a_event->IsUp() && (player->unkBDD & FlagBDD::kSprinting) != FlagBDD::kNone)
+	{
+		player->unkBDD &= ~FlagBDD::kSprinting;
+	}
+
+	using func_t = decltype(&RE::SprintHandler::ProcessButton);
+	REL::Offset<func_t> func(SprintHandler_ProcessButton);
+	return func(a_this, a_event, a_data);
+}
+
+void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
+{
+	switch (a_msg->type) {
+	case SKSE::MessagingInterface::kDataLoaded:
+		RE::UI* ui = RE::UI::GetSingleton();
+		ui->GetEventSource<RE::MenuOpenCloseEvent>()->AddEventSink(&g_menuOpenCloseEventHandler);
+		_MESSAGE("Menu open/close event handler sinked.");
+		break;
+	}
+}
+
+void ShowErrorMessage(std::string a_error, std::string a_desc = "")
+{
+	_FATALERROR(a_error.c_str());
+	int result;
+	if (a_desc != "")
+	{
+		 result = MessageBoxA(nullptr, (a_error + "\n\n" + a_desc + "\n\nPress OK to continue with the mod disabled.\nPress CANCEL to exit.").c_str(), "Classic Sprinting Redone - Error", MB_OKCANCEL | MB_ICONERROR | MB_DEFBUTTON2 | MB_SYSTEMMODAL);
+
+	}
+	else
+	{
+		 result = MessageBoxA(nullptr, (a_error + "\n\nPress OK to continue with the mod disabled.\nPress CANCEL to exit.").c_str(), "Classic Sprinting Redone - Error", MB_OKCANCEL | MB_ICONERROR | MB_DEFBUTTON2 | MB_SYSTEMMODAL);
+	}
+	if (result == IDCANCEL)
+	{
+		DWORD pid = GetCurrentProcessId();
+		HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, 0, pid);
+		TerminateProcess(hProcess, EXIT_FAILURE);
+		CloseHandle(hProcess);
+	}
+}
 
 extern "C" {
-
-	bool SKSEPlugin_Query(const SKSEInterface * a_skse, PluginInfo * a_info)
+	bool SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 	{
-		gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Skyrim Special Edition\\SKSE\\ClassicSprinting.log");
-		gLog.SetPrintLevel(IDebugLog::kLevel_Error);
-		gLog.SetLogLevel(IDebugLog::kLevel_DebugMessage);
+		SKSE::Logger::OpenRelative(FOLDERID_Documents, L"\\My Games\\Skyrim Special Edition\\SKSE\\ClassicSprintingRedone.log");
+		SKSE::Logger::SetPrintLevel(SKSE::Logger::Level::kDebugMessage);
+		SKSE::Logger::SetFlushLevel(SKSE::Logger::Level::kDebugMessage);
+		SKSE::Logger::UseLogStamp(true);
 
-		_MESSAGE("Classic Sprinting Redone (SKSE64) for Skyrim Special Edition.");
-		_MESSAGE("Initializing...");
+		_MESSAGE("Classic Sprinting Redone v%s for Skyrim Special Edition initializing...", CSR_VERSION_VERSTRING);
 
-		a_info->infoVersion = PluginInfo::kInfoVersion;
-		a_info->name = "Classic Sprinting Redone (SKSE64)";
-		a_info->version = 1;
+		a_info->infoVersion = SKSE::PluginInfo::kVersion;
+		a_info->name = "Classic Sprinting Redone";
+		a_info->version = CSR_VERSION_MAJOR;
 
-		if (a_skse->isEditor)
-		{
-			_ERROR("Loaded in editor, mod is disabled.");
+		if (a_skse->IsEditor()) {
+			_FATALERROR("Loaded in editor, marking as incompatible!");
 			return false;
 		}
-		if ((a_skse->runtimeVersion != RUNTIME_VERSION_1_5_80) && (a_skse->runtimeVersion != RUNTIME_VERSION_1_5_73) && (a_skse->runtimeVersion != RUNTIME_VERSION_1_5_97))
+
+
+		if (a_skse->RuntimeVersion() < SKSE::RUNTIME_1_5_3) {
+			ShowErrorMessage("Unsupported runtime version " + a_skse->RuntimeVersion().GetString(), "Please update your game to a more recent version to use this mod.");
+			return false;
+		}
+
+		if (SKSE::AllocTrampoline(1024 * 8))
 		{
-			_ERROR("Error: Incompatible Skyrim version.");
+			_MESSAGE("Trampoline creation successful.");
+		}
+		else {
+			ShowErrorMessage("Trampoline creation failed!", "This might be a result of too many SKSE mods being loaded. Try to disable other SKSE mods to confirm.");
+			return false;
+		}
+
+		//Check if Address Library is available
+		std::string fileName = "Data/SKSE/Plugins/version-" + std::to_string(a_skse->RuntimeVersion().GetMajor()) + "-" + std::to_string(a_skse->RuntimeVersion().GetMinor()) + "-" + std::to_string(a_skse->RuntimeVersion().GetRevision()) + "-" + std::to_string(a_skse->RuntimeVersion().GetBuild()) + ".bin";
+		if (!std::filesystem::exists(fileName))
+		{
+			ShowErrorMessage("Address Library for SKSE Plugins not found for runtime version " + a_skse->RuntimeVersion().GetString(), "This mod requires it to function. Please install it.\nIf the game just updated please wait until a new version of the library is available.");
 			return false;
 		}
 
 		return true;
 	}
 
-	bool SKSEPlugin_Load(const SKSEInterface * a_skse) {
-
-		if (!g_branchTrampoline.Create(1024 * 64))
-		{
-			_ERROR("Couldn't create branch trampoline. The mod is disabled.");
-			return false;
-		}
-		if (!g_localTrampoline.Create(1024 * 64, nullptr))
-		{
-			_ERROR("Couldn't create codegen buffer. The mod is disabled.");
+	bool SKSEPlugin_Load(SKSE::LoadInterface* a_skse)
+	{
+		if (!SKSE::Init(a_skse)) {
+			ShowErrorMessage("SKSE initialization failed!");
 			return false;
 		}
 
-		_MESSAGE("Injecting code...");
-
-		//This function keeps track of the status of some of the keys. The key is determined by the register R8. If R8 == 0x2 it's the sprinting key.
-		//The sprinting status is determined by the value at SprintStatusAddress
-
-		{
-			struct SprintKeyStatus_Code : Xbyak::CodeGenerator
-			{
-				SprintKeyStatus_Code(void * buf) : Xbyak::CodeGenerator(4096, buf)
-				{
-					Xbyak::Label keyDown;
-					Xbyak::Label newCode;
-					Xbyak::Label sprintStatus;
-					Xbyak::Label rtrn;
-					//Original code
-					xorps(xmm0, xmm0);
-					ucomiss(xmm0, ptr[rdx + 0x28]);
-					jne(keyDown);
-					comiss(xmm0, ptr[rdx + 0x2C]);
-					ja(keyDown);
-					//Key up
-					mov(eax, 0x1);
-					test(al, al);
-					sete(al);
-					mov(ptr[rcx + 0x10], al);
-					jmp(newCode);
-
-					L(keyDown);
-					//Key down
-					xor (eax, eax);
-					test(al, al);
-					sete(al);
-					mov(ptr[rcx + 0x10], al);
-					jmp(newCode);
-					L(newCode);
-					//New code
-					cmp(r8, 0x2); //Check R8 to make sure it's the sprinting button
-					jne(rtrn);
-					push(rbx);
-					mov(rbx, SprintStatusAddress.GetUIntPtr());
-					mov(rbx, ptr[rbx]);
-					mov(byte[rbx + 0xBDD], al);
-					pop(rbx);
-					ret();
-					L(rtrn);
-					ret();
-				}
-			};
-			void * codeBuf = g_localTrampoline.StartAlloc();
-			SprintKeyStatus_Code code(codeBuf);
-			g_localTrampoline.EndAlloc(code.getCurr());
-			g_branchTrampoline.Write6Branch(SprintKeyStatusFunction.GetUIntPtr(), uintptr_t(code.getCode()));
+		auto messaging = SKSE::GetMessagingInterface();
+		if (messaging->RegisterListener("SKSE", MessageHandler)) {
+			_MESSAGE("Messaging interface registration successful.");
+		}
+		else {
+			ShowErrorMessage( "Messaging interface registration failed!");
+			return false;
 		}
 
-		//Sprinting may still get "stuck" in some cases (e.g opened a menu when still holding the button).
-		//To fix this we inject this code that is executed before any control is processed.
+		_MESSAGE("Classic Sprinting Redone loaded.");
 
-		//I might remove this part in the future as it is not in the best place, and other controls have this issue anyways.
+		REL::Offset<std::uintptr_t> vTable(RE::Offset::SprintHandler::Vtbl);
+		vTable.write_vfunc(0x4, &SprintHandler_ProcessButton_Hook);
 
-		{
-			struct ControlFunction_Code : Xbyak::CodeGenerator
-			{
-				ControlFunction_Code(void * buf) : Xbyak::CodeGenerator(4096, buf)
-				{
-					Xbyak::Label returnAddress;
-					Xbyak::Label skipAssign;
-
-					//Original code
-					mov(ptr[rsp + 0x8], rbx);
-					push(rdi);
-
-					//New code
-					push(rbx);
-					push(rax);
-					mov(rax, 0x0);
-
-					mov(rbx, SprintStatusAddress.GetUIntPtr());
-					mov(rbx, ptr[rbx]);
-					cmp(rbx, 0x0); //Fix for Mod menu
-					je(skipAssign);
-					mov(ptr[rbx + 0xBDD], rax);
-					L(skipAssign);
-					pop(rbx);
-					pop(rax);
-					jmp(ptr[rip + returnAddress]);
-
-					L(returnAddress);
-					dq(ControlFunction.GetUIntPtr() + 0x6);
-				}
-			};
-
-			void * codeBuf = g_localTrampoline.StartAlloc();
-			ControlFunction_Code code(codeBuf);
-			g_localTrampoline.EndAlloc(code.getCurr());
-
-			g_branchTrampoline.Write6Branch(ControlFunction.GetUIntPtr(), uintptr_t(code.getCode()));
-		}
-
-		_MESSAGE("Classic Sprinting Redone has been successfully started.");
+		REL::SafeWrite16(SprintHandler_ProcessButton.address() + 0x16, 0x9090);
+		REL::SafeWrite8(SprintHandler_ProcessButton.address() + 0x6A, 0x0A); // xor -> or
 
 		return true;
 	}
