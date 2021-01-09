3d4484fa7eb61d228fbf453838f1c6dc5ea5aea0
Vermunds <33235701+Vermunds@users.noreply.github.com>
2021-01-09 17:57:46 +0100

    2.1.0
    Updated for the latest version of CommonLibSSE.
    Removed trampoline allocation as it's no longer necessary.
    Fixed stamina bar not flashing when running out of stamina.
    Improved compatibility with QuickLoot.

--- main.cpp
+++ main.cpp
@@ -1,47 +1,85 @@
 #include "RE/Skyrim.h"
-#include "SKSE/API.h"
-#include "REL/Relocation.h"
+#include "SKSE/SKSE.h"
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 #include "version.h"
 
-inline constexpr REL::ID SprintHandler_ProcessButton(static_cast<std::uint64_t>(41358));
+constexpr auto MESSAGE_BOX_TYPE = 0x00001010L; // MB_OK | MB_ICONERROR | MB_SYSTEMMODAL
 
-enum FlagBDD : UInt8
-{
-	kNone = 0,
-	kSprinting = 1 << 0
-	//...
-};
+using namespace SKSE::util;
 
-class MenuOpenCloseEventHandler : public RE::BSTEventSink<RE::MenuOpenCloseEvent>
+namespace CSR
 {
-public:
-	virtual RE::BSEventNotifyControl ProcessEvent(const RE::MenuOpenCloseEvent* a_event, RE::BSTEventSource<RE::MenuOpenCloseEvent>* a_dispatcher) override
+	using FlagBDD = RE::PlayerCharacter::FlagBDD;
+
+	void FlashHudMenuMeter(std::uint32_t a_meter)
+	{
+		using func_t = decltype(&FlashHudMenuMeter);
+		REL::Relocation<func_t> func(REL::ID{ 51907 });
+		return func(a_meter);
+	}
+
+	class MenuOpenCloseEventHandler : public RE::BSTEventSink<RE::MenuOpenCloseEvent>
+	{
+	public:
+		virtual RE::BSEventNotifyControl ProcessEvent(const RE::MenuOpenCloseEvent* a_event, RE::BSTEventSource<RE::MenuOpenCloseEvent>* a_dispatcher) override
+		{
+			RE::PlayerCharacter* player = RE::PlayerCharacter::GetSingleton();
+
+			if (a_event && a_event->opening && a_event->menuName != "LootMenu" && (player->unkBDD & FlagBDD::kSprinting) != FlagBDD::kNone)
+			{
+				// Menu was opened, stop sprinting
+				player->unkBDD &= ~FlagBDD::kSprinting;
+			}
+
+			return RE::BSEventNotifyControl::kContinue;
+		}
+	};
+	MenuOpenCloseEventHandler g_menuOpenCloseEventHandler;
+
+	void SprintHandler_ProcessButton_Hook(RE::SprintHandler* a_this, RE::ButtonEvent* a_event, RE::PlayerControlsData* a_data)
 	{
 		RE::PlayerCharacter* player = RE::PlayerCharacter::GetSingleton();
+		float stamina = player->GetActorValue(RE::ActorValue::kStamina);
 
-		if (a_event && a_event->opening && (player->unkBDD & FlagBDD::kSprinting) != FlagBDD::kNone)
+		if (a_event->IsDown())
 		{
-			player->unkBDD &= ~FlagBDD::kSprinting;
+			if (stamina > 0.0f)
+			{
+				if ((player->unkBDD & FlagBDD::kSprinting) == FlagBDD::kNone)
+				{
+					// If not sprinting, start sprinting
+					player->unkBDD |= FlagBDD::kSprinting;
+				}
+			}
+			else
+			{
+				// Out of stamina, flash stamina meter
+				FlashHudMenuMeter(26);
+			}
 		}
-
-		return RE::BSEventNotifyControl::kContinue;
+		else if (a_event->IsUp())
+		{
+			if ((player->unkBDD & FlagBDD::kSprinting) != FlagBDD::kNone)
+			{
+				// If sprinting, stop sprinting
+				player->unkBDD &= ~FlagBDD::kSprinting;
+			}
+		}
+		else if (a_event->IsPressed())
+		{
+			if (stamina > 0.0f)
+			{
+				if ((player->unkBDD & FlagBDD::kSprinting) == FlagBDD::kNone)
+				{
+					// If not sprinting, start sprinting
+					player->unkBDD |= FlagBDD::kSprinting;
+				}
+			}
+		}
 	}
-};
-MenuOpenCloseEventHandler g_menuOpenCloseEventHandler;
-
-void SprintHandler_ProcessButton_Hook(RE::SprintHandler* a_this, RE::ButtonEvent* a_event, RE::PlayerControlsData* a_data)
-{
-	RE::PlayerCharacter* player = RE::PlayerCharacter::GetSingleton();
-
-	if (a_event->IsUp() && (player->unkBDD & FlagBDD::kSprinting) != FlagBDD::kNone)
-	{
-		player->unkBDD &= ~FlagBDD::kSprinting;
-	}
-
-	using func_t = decltype(&RE::SprintHandler::ProcessButton);
-	REL::Offset<func_t> func(SprintHandler_ProcessButton);
-	return func(a_this, a_event, a_data);
 }
 
 void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
@@ -49,73 +87,40 @@ void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
 	switch (a_msg->type) {
 	case SKSE::MessagingInterface::kDataLoaded:
 		RE::UI* ui = RE::UI::GetSingleton();
-		ui->GetEventSource<RE::MenuOpenCloseEvent>()->AddEventSink(&g_menuOpenCloseEventHandler);
-		_MESSAGE("Menu open/close event handler sinked.");
+		ui->GetEventSource<RE::MenuOpenCloseEvent>()->AddEventSink(&CSR::g_menuOpenCloseEventHandler);
+		SKSE::log::info("Menu open/close event handler sinked.");
 		break;
-	}
-}
-
-void ShowErrorMessage(std::string a_error, std::string a_desc = "")
-{
-	_FATALERROR(a_error.c_str());
-	int result;
-	if (a_desc != "")
-	{
-		 result = MessageBoxA(nullptr, (a_error + "\n\n" + a_desc + "\n\nPress OK to continue with the mod disabled.\nPress CANCEL to exit.").c_str(), "Classic Sprinting Redone - Error", MB_OKCANCEL | MB_ICONERROR | MB_DEFBUTTON2 | MB_SYSTEMMODAL);
-
-	}
-	else
-	{
-		 result = MessageBoxA(nullptr, (a_error + "\n\nPress OK to continue with the mod disabled.\nPress CANCEL to exit.").c_str(), "Classic Sprinting Redone - Error", MB_OKCANCEL | MB_ICONERROR | MB_DEFBUTTON2 | MB_SYSTEMMODAL);
-	}
-	if (result == IDCANCEL)
-	{
-		DWORD pid = GetCurrentProcessId();
-		HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, 0, pid);
-		TerminateProcess(hProcess, EXIT_FAILURE);
-		CloseHandle(hProcess);
 	}
 }
 
 extern "C" {
 	bool SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 	{
-		SKSE::Logger::OpenRelative(FOLDERID_Documents, L"\\My Games\\Skyrim Special Edition\\SKSE\\ClassicSprintingRedone.log");
-		SKSE::Logger::SetPrintLevel(SKSE::Logger::Level::kDebugMessage);
-		SKSE::Logger::SetFlushLevel(SKSE::Logger::Level::kDebugMessage);
-		SKSE::Logger::UseLogStamp(true);
+		assert(SKSE::log::log_directory().has_value());
+		auto path = SKSE::log::log_directory().value() / std::filesystem::path("ClassicSprintingRedone.log");
+		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path.string(), true);
+		auto log = std::make_shared<spdlog::logger>("global log", std::move(sink));
 
-		_MESSAGE("Classic Sprinting Redone v%s for Skyrim Special Edition initializing...", CSR_VERSION_VERSTRING);
+		log->set_level(spdlog::level::trace);
+		log->flush_on(spdlog::level::trace);
+
+		spdlog::set_default_logger(std::move(log));
+		spdlog::set_pattern("%g(%#): [%^%l%$] %v", spdlog::pattern_time_type::local);
+
+		SKSE::log::info("Classic Sprinting Redone v" + std::string(CSR_VERSION_VERSTRING) + " - (" + std::string(__TIMESTAMP__) + ")");
 
 		a_info->infoVersion = SKSE::PluginInfo::kVersion;
 		a_info->name = "Classic Sprinting Redone";
 		a_info->version = CSR_VERSION_MAJOR;
 
 		if (a_skse->IsEditor()) {
-			_FATALERROR("Loaded in editor, marking as incompatible!");
+			SKSE::log::critical("Loaded in editor, marking as incompatible!");
 			return false;
 		}
 
-
-		if (a_skse->RuntimeVersion() < SKSE::RUNTIME_1_5_3) {
-			ShowErrorMessage("Unsupported runtime version " + a_skse->RuntimeVersion().GetString(), "Please update your game to a more recent version to use this mod.");
-			return false;
-		}
-
-		if (SKSE::AllocTrampoline(1024 * 8))
-		{
-			_MESSAGE("Trampoline creation successful.");
-		}
-		else {
-			ShowErrorMessage("Trampoline creation failed!", "This might be a result of too many SKSE mods being loaded. Try to disable other SKSE mods to confirm.");
-			return false;
-		}
-
-		//Check if Address Library is available
-		std::string fileName = "Data/SKSE/Plugins/version-" + std::to_string(a_skse->RuntimeVersion().GetMajor()) + "-" + std::to_string(a_skse->RuntimeVersion().GetMinor()) + "-" + std::to_string(a_skse->RuntimeVersion().GetRevision()) + "-" + std::to_string(a_skse->RuntimeVersion().GetBuild()) + ".bin";
-		if (!std::filesystem::exists(fileName))
-		{
-			ShowErrorMessage("Address Library for SKSE Plugins not found for runtime version " + a_skse->RuntimeVersion().GetString(), "This mod requires it to function. Please install it.\nIf the game just updated please wait until a new version of the library is available.");
+		if (a_skse->RuntimeVersion() < SKSE::RUNTIME_1_5_39) {
+			SKSE::log::critical("Unsupported runtime version " + a_skse->RuntimeVersion().string());
+			SKSE::WinAPI::MessageBox(nullptr, std::string("Unsupported runtime version " + a_skse->RuntimeVersion().string()).c_str(), "Classic Sprinting Redone - Error", MESSAGE_BOX_TYPE);
 			return false;
 		}
 
@@ -124,29 +129,25 @@ extern "C" {
 
 	bool SKSEPlugin_Load(SKSE::LoadInterface* a_skse)
 	{
-		if (!SKSE::Init(a_skse)) {
-			ShowErrorMessage("SKSE initialization failed!");
-			return false;
-		}
+		SKSE::Init(a_skse);
 
 		auto messaging = SKSE::GetMessagingInterface();
 		if (messaging->RegisterListener("SKSE", MessageHandler)) {
-			_MESSAGE("Messaging interface registration successful.");
+			SKSE::log::info("Messaging interface registration successful.");
 		}
 		else {
-			ShowErrorMessage( "Messaging interface registration failed!");
+			SKSE::log::critical("Messaging interface registration failed.");
+			SKSE::WinAPI::MessageBox(nullptr, "Messaging interface registration failed.", "Classic Sprinting Redone - Error", MESSAGE_BOX_TYPE);
 			return false;
 		}
 
-		_MESSAGE("Classic Sprinting Redone loaded.");
+		SKSE::log::info("Classic Sprinting Redone loaded.");
 
-		REL::Offset<std::uintptr_t> vTable(RE::Offset::SprintHandler::Vtbl);
-		vTable.write_vfunc(0x4, &SprintHandler_ProcessButton_Hook);
+		REL::Relocation<std::uintptr_t> vTable(RE::Offset::SprintHandler::Vtbl);
+		vTable.write_vfunc(0x4, &CSR::SprintHandler_ProcessButton_Hook);
 
-		REL::SafeWrite16(SprintHandler_ProcessButton.address() + 0x16, 0x9090);
-		REL::SafeWrite8(SprintHandler_ProcessButton.address() + 0x6A, 0x0A); // xor -> or
+		SKSE::log::info("Hooks Installed.");
 
 		return true;
 	}
-
 };
