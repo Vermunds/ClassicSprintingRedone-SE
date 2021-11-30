5811e79afc2344409abe768c770b76560fdd49b8
Vermunds <33235701+Vermunds@users.noreply.github.com>
2021-11-30 14:35:34 +0100

    AE update

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 
 #include "version.h"
 
-constexpr auto MESSAGE_BOX_TYPE = 0x00001010L;  // MB_OK | MB_ICONERROR | MB_SYSTEMMODAL
-
 namespace CSR
 {
 	using FlagBDD = RE::PlayerCharacter::FlagBDD;
@@ -12,7 +10,7 @@ namespace CSR
 	void FlashHudMenuMeter(std::uint32_t a_meter)
 	{
 		using func_t = decltype(&FlashHudMenuMeter);
-		REL::Relocation<func_t> func(REL::ID{ 51907 });
+		REL::Relocation<func_t> func(REL::ID{ 52845 });
 		return func(a_meter);
 	}
 
@@ -90,7 +88,20 @@ void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
 }
 
 extern "C" {
-	DLLEXPORT bool SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
+
+	DLLEXPORT SKSE::PluginVersionData SKSEPlugin_Version = []() {
+		SKSE::PluginVersionData v{};
+		v.PluginVersion(REL::Version{ Version::MAJOR, Version::MINOR, Version::PATCH, 0 });
+		v.PluginName(Version::PROJECT);
+		v.AuthorName("Vermunds"sv);
+		v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
+
+		v.addressLibrary = true;
+		v.sigScanning = false;
+		return v;
+	}();
+
+	DLLEXPORT bool SKSEPlugin_Load(SKSE::LoadInterface* a_skse)
 	{
 		assert(SKSE::log::log_directory().has_value());
 		auto path = SKSE::log::log_directory().value() / std::filesystem::path("ClassicSprintingRedone.log");
@@ -105,28 +116,12 @@ extern "C" {
 
 		SKSE::log::info("Classic Sprinting Redone v" + std::string(Version::NAME) + " - (" + std::string(__TIMESTAMP__) + ")");
 
-		a_info->infoVersion = SKSE::PluginInfo::kVersion;
-		a_info->name = Version::PROJECT.data();
-		a_info->version = Version::MAJOR;
-
 		if (a_skse->IsEditor())
 		{
 			SKSE::log::critical("Loaded in editor, marking as incompatible!");
 			return false;
 		}
 
-		if (a_skse->RuntimeVersion() < SKSE::RUNTIME_1_5_39)
-		{
-			SKSE::log::critical("Unsupported runtime version " + a_skse->RuntimeVersion().string());
-			SKSE::WinAPI::MessageBox(nullptr, std::string("Unsupported runtime version " + a_skse->RuntimeVersion().string()).c_str(), "Classic Sprinting Redone - Error", MESSAGE_BOX_TYPE);
-			return false;
-		}
-
-		return true;
-	}
-
-	DLLEXPORT bool SKSEPlugin_Load(SKSE::LoadInterface* a_skse)
-	{
 		SKSE::Init(a_skse);
 
 		auto messaging = SKSE::GetMessagingInterface();
@@ -137,20 +132,17 @@ extern "C" {
 		else
 		{
 			SKSE::log::critical("Messaging interface registration failed.");
-			SKSE::WinAPI::MessageBox(nullptr, "Messaging interface registration failed.", "Classic Sprinting Redone - Error", MESSAGE_BOX_TYPE);
 			return false;
 		}
-
-		SKSE::log::info("Classic Sprinting Redone loaded.");
 
 		REL::Relocation<std::uintptr_t> vTable(RE::Offset::SprintHandler::Vtbl);
 		vTable.write_vfunc(0x4, &CSR::SprintHandler_ProcessButton_Hook);
 
 		// Force sprint state to sync in every frame
-		REL::safe_write(REL::ID{ 39673 }.address() + 0x15A, std::uint16_t(0x9090));
+		REL::safe_write(REL::ID{ 40760 }.address() + 0x159, std::uint16_t(0x9090));
 
 		// Skip HUD meter flashing when out of stamina - we handle it ourselves
-		REL::safe_write(REL::ID{ 41271 }.address() + 0x30C, std::uint8_t(0xEB));
+		REL::safe_write(REL::ID{ 42350 }.address() + 0x350, std::uint8_t(0xEB));
 
 		SKSE::log::info("Hooks Installed.");
 
